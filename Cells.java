5bb16c5f7b5360ae96bf9a8d4046ee388fbef6bb
Dr.Ferdinand <shagrov.nikita@mail.ru>
2021-05-25 11:19:10 +0300

    Finally

--- Cells.java
+++ Cells.java
@@ -1,35 +1,33 @@
 import java.awt.*;
 import javax.swing.*;
-import java.io.*;
-import java.awt.event.*;
 import java.util.*;
-
-public class Cells extends JComponent
+ 
+public  class Cells extends JComponent
 {  
    private boolean open = false; 
    private boolean bomb = false;
    private boolean flag = false;
    private boolean bang = false;
    private boolean won = false;
+ 
    private static boolean end = false;
-
    private static int rows;
    private static int bombs;
    private static int sizeOfSqrt = 30;
    private static  int countOpenCells = 0;
-
-   private  int countBombNear = 0;  
+ 
+   private  int countBombNear;  
    private int x;
    private int y;
-
+ 
    private static Color[] color = { Color.BLUE, Color.GREEN, Color.RED, Color.MAGENTA, Color.PINK};
-
+ 
    public void setFieldSize(int rows1, int bombs1)
    {
       bombs = bombs1;
       rows = rows1;
    }
-
+ 
    public boolean isOpen() 
    {
     return open;
@@ -38,56 +36,70 @@ public class Cells extends JComponent
    {
       return bomb;
    }
-
+ 
    public void setBomb()
    {
       bomb = true;
    } 
-
+ 
    public void isFlag()
    {
       flag = !flag;
    }
-
+ 
    public void setCountNearBomb (int count)
    {
       countBombNear = count;
    }
-   
+ 
    public int getCountNearBomb ()
    {
       return countBombNear;
    }
-
+ 
    public boolean getWon ()
    {
       return won;
    }
-
+ 
    public boolean getFail ()
    {  
       return bang;
    }
-
+ 
    public boolean End (boolean _end)
    {
       return end = _end;
    }
-
+ 
    public void refresh()
    {
-       end = false;
-       countOpenCells = 0;  
+      end = false;
+      countOpenCells = 0;  
    }
+ 
    public void openCell()
    {
       open = true;
       bang =  bomb;
       countOpenCells++;
       if(countOpenCells >= rows*rows-bombs){ won = true;}
-      
+ 
    }
-
+ 
+   private void paintflag (Graphics g, String str)
+   {
+      g.setColor(Color.red);
+      g.setFont(new Font("", Font.BOLD, sizeOfSqrt));
+      g.drawString(str, x*sizeOfSqrt + 8, y*sizeOfSqrt + 26);
+   }
+ 
+   public void setCoord(int x1,  int y2)
+   {
+      x = x1;
+      y = y2;
+   }
+ 
    private void paintBomb(Graphics g) 
    {  
       g.setColor(Color.BLACK);
@@ -97,7 +109,7 @@ public class Cells extends JComponent
       g.setColor(Color.WHITE);
       g.fillRect(x*sizeOfSqrt + 11, y*sizeOfSqrt + 10, 4, 4); 
    }
-
+ 
    private void paintBang(Graphics g)
    {  
       g.setColor(Color.RED);
@@ -107,32 +119,19 @@ public class Cells extends JComponent
       g.setColor(Color.WHITE);
       g.fillRect(x*sizeOfSqrt + 11, y*sizeOfSqrt + 10, 4, 4);  
    }
-
+ 
    private void paintString (Graphics g, int num)
    { 
       g.setColor(color [countBombNear - 1]);
       g.setFont(new Font("", Font.BOLD, sizeOfSqrt));
       g.drawString(Integer.toString(num), x*sizeOfSqrt + 8, y*sizeOfSqrt + 26);
    }
-
-   private void paintflag (Graphics g, String str)
-   {
-      g.setColor(Color.red);
-      g.setFont(new Font("", Font.BOLD, sizeOfSqrt));
-      g.drawString(str, x*sizeOfSqrt + 8, y*sizeOfSqrt + 26);
-   }
-
-   public void setCoord(int x1,  int y2)
-   {
-      x = x1;
-      y = y2;
-   }
-
+ 
    protected void paintComponent(Graphics g)
     {    
         g.setColor(Color.lightGray);
         g.drawRect(x*sizeOfSqrt, y*sizeOfSqrt, sizeOfSqrt, sizeOfSqrt);
-
+ 
         if (!open)
         {
           if (isBombed() && end == true) paintBomb(g);
@@ -149,5 +148,6 @@ public class Cells extends JComponent
                         if (countBombNear > 0)
                         paintString(g, countBombNear);
       } 
-    
+ 
 }   
+
--- Miner.java
+++ Miner.java
@@ -2,178 +2,276 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*; // Адаптер мыши
 import java.util.*; //Рандом
-
+ 
 class  Miner
 {   
-    int x = 0;
-    int y = 0;
-    int countBomb = 10;
-    int sizeOfFeild = 8;
-    boolean end = false;
-    Cells[][] map = new Cells[sizeOfFeild][sizeOfFeild];
+    private int x;
+    private int y;
+    private int countBomb = 10;
+    private int sizeOfFeild = 8;
+    private boolean end = false;
+ 
+    Cells[][] map; 
+ 
+    Choice choise = new Choice();
+    JFrame setDifficult = new JFrame("Miner");
+ 
     JFrame frame = new JFrame("Miner"); 
-    Panel panel = new Panel();
+ 
+    Panel panel;
     Random random = new Random();
-    
-
+ 
+ 
     JButton restartButton = new JButton("Restart");
-
+ 
+ 
     public static void main (String[]  args)
     {
         new Miner();
     }
-
+ 
     private  Miner() 
     {   
-        
-        panelInit();
-        
+        diffInit();  
     }
-
-    private void panelInit()
+    private void diffInit()
     {   
-
-        panel.setPreferredSize(new Dimension(30*sizeOfFeild, 30*sizeOfFeild));
-
-        panel.setBackground(Color.WHITE);
-
-        frame.setLocationRelativeTo(null);
+        JPanel firstPanel = new JPanel();
+ 
+        JLabel label = new JLabel("Please, choise difficult:");
+ 
+        JButton buttonOk = new JButton("OK");
+ 
+        choise.add("Easy");
+        choise.add("Medium");
+        choise.add("Hard");
+ 
+        choise.addItemListener(choiseListener);
+        buttonOk.addActionListener(listenerOk);
+ 
+        setDifficult.setLocationRelativeTo(null);
+        setDifficult.setVisible (true);
+        setDifficult.setResizable (false);
+ 
+        setDifficult.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+ 
+        setDifficult.setIconImage(getImage("icon"));
+ 
+        setDifficult.add(BorderLayout.CENTER, firstPanel);
+ 
+        firstPanel.add(label);
+        firstPanel.add(choise);
+        firstPanel.add(buttonOk);
+ 
+        setDifficult.pack();
+ 
+    }
+ 
+    private void frameInit()
+    {   
+ 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable (false);
-        
-      
-        panel.addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mouseReleased(MouseEvent e)
-            {   
-                super.mouseReleased(e);
-                
-                x = e.getX()/30;
-                y = e.getY()/30;
-
-                if (e.getButton() == MouseEvent.BUTTON1)
-                    {
-                      recursiveOpener(x,y);
-                      if (map[x][y].getWon() || map[x][y].getFail()){end = true;}
-                    }
-                if (e.getButton() == MouseEvent.BUTTON3) map[x][y].isFlag();
-                if (end == true) map[x][y].End(end);
-
-                 panel.repaint();
-                if (end == true) panel.removeMouseListener(this);
-                }
-        });
-
-        restartButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)
-            {   
-                
-                panelInit();
-                end = false;
-                map[x][y].refresh();
-                
-            }
-        });
-
+ 
         frame.add(BorderLayout.SOUTH, restartButton);
         frame.add(BorderLayout.CENTER, panel);
-        frame.setVisible (true);
+ 
+ 
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+ 
         frame.setIconImage(getImage("icon"));
+ 
         frame.pack();
-
+    }
+ 
+    private void panelInit()
+    {   
+ 
+        panel = new Panel();
+ 
+        panel.setPreferredSize(new Dimension(30 * sizeOfFeild, 30 * sizeOfFeild));
+ 
+        panel.setBackground(Color.WHITE);
+ 
+        panel.addMouseListener(mouse); 
+ 
+        restartButton.addActionListener(buttonListener);
+ 
         createCell();
+ 
         map[0][0].setFieldSize(sizeOfFeild,countBomb);
     }
-   
-
+ 
     private void recursiveOpener (int q, int v)
     {   if ((q < 0 || q > sizeOfFeild -1 || v < 0 || v > sizeOfFeild - 1)) return;
         if (map[q][v].isOpen() == false)
              {
                 map[q][v].openCell();
              } else return;
-
+ 
         if (map[q][v].getCountNearBomb() > 0) return;
-
+ 
         for (int x1 = -1; x1 < 2; x1++)
             for (int y1 = -1; y1 < 2; y1++)
                 recursiveOpener(q + x1, v + y1);
     }
-
+ 
     private void createCell()
     { 
-        int x,y;
-        for (x = 0; x < sizeOfFeild ; x++)
+        map = new Cells[sizeOfFeild][sizeOfFeild];
+        int i,j;
+        for (i = 0; i < sizeOfFeild ; i++)
         {
-            for (y = 0; y < sizeOfFeild; y++)
+            for (j = 0; j < sizeOfFeild; j++)
                 {
-                    map[x][y] = new Cells();
-                    map[x][y].setCoord(x,y);
+                    map[i][j] = new Cells();
+                    map[i][j].setCoord(i,j);
                 }
         }
-
+ 
         for (int planted = 0; planted < countBomb; planted++)
         {  
             do 
             {
-              x = random.nextInt(sizeOfFeild);
-              y = random.nextInt(sizeOfFeild);
-            } while (map[x][y].isBombed());
-            map[x][y].setBomb();
+              i = random.nextInt(sizeOfFeild);
+              j = random.nextInt(sizeOfFeild);
+            } while (map[i][j].isBombed());
+            map[i][j].setBomb();
         }
-
-
-        for (x = 0; x < sizeOfFeild; x++)
+ 
+ 
+        for (i = 0; i < sizeOfFeild; i++)
         {
-            for (y = 0; y < sizeOfFeild; y++)
+            for (j = 0; j < sizeOfFeild; j++)
             {
-               if (!map[x][y].isBombed())
+               if (!map[i][j].isBombed())
                {
                    int count = 0;
                    for (int x1 = -1; x1 < 2; x1++)
                    {
                         for (int y1 = -1; y1 < 2; y1++)
                         {
-                            int dx = x + x1;
-                            int dy = y + y1;
+                            int dx = i + x1;
+                            int dy = j + y1;
                             if (dx < 0 || dy < 0 || dx > sizeOfFeild - 1 || dy > sizeOfFeild - 1) 
                             {
-                              dx = x;
-                              dy = y;    
+                              dx = i;
+                              dy = j;    
                             }
                             if (map[dx][dy].isBombed())
                                 {
                                 count++;
                                 } 
                         }       
-                        map[x][y].setCountNearBomb(count);
+                        map[i][j].setCountNearBomb(count);
                     }    
                 }    
             }
         }
     }    
-
-
+ 
     private Image getImage (String name)
     {
         String filename = "res/" + name.toLowerCase() + ".png"; 
         ImageIcon icon = new ImageIcon (getClass().getResource(filename));
         return icon.getImage();
     }
-
-    class Panel extends JPanel
+ 
+    public class Panel extends JPanel
     {   
        @Override
         public void paint(Graphics g) 
         {
             super.paint(g);
-            for (int x = 0; x < sizeOfFeild ; x++)
-                for (int y = 0; y < sizeOfFeild; y++) 
-                    map[y][x].paintComponent(g);
+            for (int i = 0; i < sizeOfFeild ; i++)
+                for (int j = 0; j < sizeOfFeild; j++) 
+                    map[i][j].paintComponent(g);
         }
     }
-}   
+ 
+    MouseListener mouse = new MouseAdapter() 
+        {   
+            @Override
+            public void mouseReleased(MouseEvent e) 
+            {   
+                super.mouseReleased(e);
+ 
+                x = e.getX()/30;
+                y = e.getY()/30;
+ 
+                if (e.getButton() == MouseEvent.BUTTON1)
+                    {
+                      recursiveOpener(x,y);
+                      if (map[x][y].getWon() || map[x][y].getFail()){end = true;}
+                    }
+                if (e.getButton() == MouseEvent.BUTTON3) { map[x][y].isFlag();}
+                if (end == true)
+                { 
+                    map[x][y].End(end);
+                    panel.removeMouseListener(mouse);
+                }
+            panel.repaint();
+            }
+        };
+ 
+        ItemListener choiseListener = new ItemListener()
+        {
+            @Override
+            public void itemStateChanged(ItemEvent ie)
+            {
+                if (ie.getItem() == "Easy")
+                {
+                sizeOfFeild = 8;
+                countBomb = 10;
+                };
+                if (ie.getItem() == "Medium")
+                {
+                sizeOfFeild = 16;
+                countBomb = 52; 
+                };
+            if (ie.getItem() == "Hard")
+                {
+                sizeOfFeild = 20;
+                countBomb = 80; 
+                };
+            }
+        };
+ 
+        ActionListener buttonListener = new ActionListener() 
+        {   
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {   
+                x = 0;
+                y = 0;
+                if (end == true) 
+                {
+                    end = false;
+                    panel.addMouseListener(mouse);
+                }
+                map[x][y].refresh();
+                createCell();
+                panel.repaint();
+ 
+            }
+        };
+ 
+        ActionListener listenerOk = new ActionListener() 
+        {   
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {   
+                setDifficult.dispose(); 
+                panelInit();
+                frameInit();
+                panel.repaint();
+ 
+            }
+        };
+ 
+}
+
 
 
 
